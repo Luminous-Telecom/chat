c4ff5b749e325f98e6f91cd1ec4cb20231c55b7e
Lucas Matheus <lucasmidiadivulgacoes@gmail.com>
2025-06-08 20:29:19 -0300

    chore: update ecosystem config and gitignore

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,16 +1,19 @@
 module.exports = [
   {
     script: "node_modules/.bin/ts-node-dev",
-    args: "--inspect=9229 --respawn --transpile-only --ignore node_modules src/server.ts",
+    args: "--respawn --transpile-only --ignore node_modules src/server.ts",
     name: "izing-backend-dev",
     exec_mode: "fork",
     instances: 1,
     env: {
-      NODE_ENV: "development"
+      NODE_ENV: "development",
+      NODE_OPTIONS: "--max-old-space-size=4096"
     },
     error_file: "./logs/pm2-error.log",
     out_file: "./logs/pm2-out.log",
     log_file: "./logs/pm2-combined.log",
-    time: true
+    time: true,
+    max_memory_restart: "1G",
+    node_args: "--max-old-space-size=4096"
   }
 ];
