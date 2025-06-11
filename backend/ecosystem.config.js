d8e978c43c55e3668e77bd7a9bed0d1135acdddc
Lucas Matheus <lucasmidiadivulgacoes@gmail.com>
2025-06-11 10:10:55 -0300

    feat: configuração do PM2 para desenvolvimento e produção

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,10 +1,36 @@
-module.exports = [
-  {
-    script: "dist/server.js",
-    name: "izing-backend",
-    exec_mode: "cluster",
-    cron_restart: "00 00 * * *",
-    instances: 1,
-    watch: false
+module.exports = {
+  apps: [
+    {
+      name: "izing-backend-dev",
+      script: 'npm',
+      args: 'run dev:server',
+      exec_mode: 'fork',
+      instances: 1,
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      },
+      error_file: './logs/pm2-error-dev.log',
+      out_file: './logs/pm2-out-dev.log',
+      log_file: './logs/pm2-combined-dev.log',
+      time: true
+    }
+  ],
+  deploy: {
+    production: {
+      name: "izing-backend-prod",
+      script: "dist/server.js",
+      exec_mode: "cluster",
+      instances: "max",
+      watch: false,
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/pm2-error-prod.log',
+      out_file: './logs/pm2-out-prod.log',
+      log_file: './logs/pm2-combined-prod.log',
+      time: true,
+      cron_restart: "00 00 * * *"
+    }
   }
-];
+};
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,33 +1,37 @@
-module.exports = [
-  {
-    script: 'npm',
-    args: 'run dev',
-    name: 'izing-frontend-vue-dev',
-    exec_mode: 'fork',
-    instances: 1,
-    watch: false,
-    watch_delay: 1000,
-    ignore_watch: [
-      'node_modules',
-      'dist',
-      'build',
-      '.quasar',
-      '*.log',
-      '.git',
-      'coverage',
-      '.nyc_output',
-      'public'
-    ],
-    env: {
-      NODE_ENV: 'production'
-    },
-    env_development: {
-      NODE_ENV: 'development'
-    },
-    error_file: './logs/pm2-error.log',
-    out_file: './logs/pm2-out.log',
-    log_file: './logs/pm2-combined.log',
-    time: true,
-    restart_delay: 4000
+module.exports = {
+  apps: [
+    {
+      name: "izing-frontend-dev",
+      script: 'npm',
+      args: 'run dev',
+      exec_mode: 'fork',
+      instances: 1,
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      },
+      error_file: './logs/pm2-error-dev.log',
+      out_file: './logs/pm2-out-dev.log',
+      log_file: './logs/pm2-combined-dev.log',
+      time: true
+    }
+  ],
+  deploy: {
+    production: {
+      name: "izing-frontend-prod",
+      script: 'npm',
+      args: 'run start',
+      exec_mode: 'cluster',
+      instances: "max",
+      watch: false,
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: './logs/pm2-error-prod.log',
+      out_file: './logs/pm2-out-prod.log',
+      log_file: './logs/pm2-combined-prod.log',
+      time: true,
+      cron_restart: "00 00 * * *"
+    }
   }
-]
+};
