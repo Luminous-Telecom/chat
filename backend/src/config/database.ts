5a09f7f08767cbc100524d11fc76b660d9d55d01
lucas matheus <lucasmidiadivulgacoes@gmail.com>
2025-06-15 03:10:14 -0300

    feat: configuração do banco de dados e usuário chat

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,23 +1,26 @@
-require("../app/config-env");
+import { SequelizeOptions } from 'sequelize-typescript';
+import { Dialect } from 'sequelize';
 
-module.exports = {
+const dbConfig: SequelizeOptions = {
   define: {
-    charset: "utf8mb4",
-    collate: "utf8mb4_bin"
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_bin'
     // freezeTableName: true
   },
   pool: {
-    max: process.env.POSTGRES_POOL_MAX || 100,
-    min: process.env.POSTGRES_POOL_MIN || 10,
-    acquire: process.env.POSTGRES_POOL_ACQUIRE || 30000,
-    idle: process.env.POSTGRES_POOL_IDLE || 10000
+    max: 100,
+    min: 10,
+    acquire: 30000,
+    idle: 10000
   },
-  dialect: process.env.DB_DIALECT || "postgres",
-  timezone: "UTC",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: process.env.DB_PORT || "5432",
-  database: process.env.POSTGRES_DB || "wchats",
-  username: process.env.POSTGRES_USER || "postgres",
-  password: process.env.POSTGRES_PASSWORD || "marina@0509",
+  dialect: 'postgres' as Dialect,
+  timezone: 'UTC',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.POSTGRES_DB || 'chat',
+  username: process.env.POSTGRES_USER || 'chat',
+  password: process.env.POSTGRES_PASSWORD || 'chat123',
   logging: false
 };
+
+module.exports = dbConfig;
