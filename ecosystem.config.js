35d2a9584d3d33464ddcb1f4140d5c226388d0fe
lucas matheus <lucasmidiadivulgacoes@gmail.com>
2025-06-16 10:00:22 -0300

    refactor: simplifica ecosystem.config.js para ambiente de desenvolvimento

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: 'chat-backend-prod',
-      script: './dist/server.js',
-      cwd: './backend',
-      instances: 'max',
-      exec_mode: 'cluster',
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3000
-      },
-      watch: false,
-      max_memory_restart: '1G',
-      exp_backoff_restart_delay: 100
-    },
-    {
       name: 'chat-backend-dev',
       script: './dist/server.js',
       cwd: './backend',
@@ -30,18 +16,6 @@ module.exports = {
       watch_options: {
         followSymlinks: false
       }
-    },
-    {
-      name: 'chat-frontend-prod',
-      script: 'serve',
-      env_production: {
-        NODE_ENV: 'production',
-        PM2_SERVE_PATH: './frontend/build',
-        PM2_SERVE_PORT: 80,
-        PM2_SERVE_SPA: 'true',
-        PM2_SERVE_HOMEPAGE: '/index.html'
-      },
-      watch: false
     },
     {
       name: 'chat-frontend-dev',
