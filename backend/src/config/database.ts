fae4a61752e12250bb30d2132665af58b80edeb0
lucas <lucas@example.com>
2025-07-01 20:12:43 -0300

    remove os falback de database

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -15,11 +15,11 @@ const dbConfig: SequelizeOptions = {
   },
   dialect: "postgres" as Dialect,
   timezone: "UTC",
-  host: process.env.POSTGRES_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.POSTGRES_DB || "chat",
-  username: process.env.POSTGRES_USER || "chat",
-  password: process.env.POSTGRES_PASSWORD || "chat123",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   logging: false,
 };
 
